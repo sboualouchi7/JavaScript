3b13228d88cc4c45aa84a0b4d3bd0be834ee2d5d
sboualouchi7 <salmanboualouchi@gmail.com>
2025-03-24 13:35:53 +0000

    day-1 && ex-5

---
+++ Day-1/ex-5.js
@@ -0,0 +1,15 @@
+//family object
+const family = {
+    father: "Hamid",
+    mother: "Khadija",
+    son: "Salman",
+    daughter: "Doha"
+};
+
+
+//key is x and value is family[x]
+
+for(let x in family){
+    console.log(x +" : "+ family[x])
+}
+
