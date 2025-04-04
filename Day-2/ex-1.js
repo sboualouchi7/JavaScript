9b035dbffdf9dc0e007f3e7390ae8adb71a05b48
sboualouchi7 <salmanboualouchi@gmail.com>
2025-04-04 10:26:49 +0000

    Day-2 && ex 1

--- Day-2/ex-1.js
+++ Day-2/ex-1.js
@@ -1,81 +1,12 @@
+const colors = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
 
-// function funcOne() {
-//     let a = 5;
-//     if(a > 1) {
-//         a = 3;
-//     }
-//     alert(`inside the funcOne function ${a}`);
-// }
-
-// // #1.1 - run in the console:
-// funcOne()
-// #1.2 
-//==>
-//On aurait une erreur quand on essaie de réassigner a , car les variables const ne peuvent pas être réassignées après leur initialisation.
-
-
-
-// //#2
-// let a = 0;
-// function funcTwo() {
-//     a = 5;
-// }
-
-// function funcThree() {
-//     alert(`inside the funcThree function ${a}`);
-// }
-
-// // #2.1 - run in the console:
-// funcThree()
-// funcTwo()
-// funcThree()
-// // #2.2 What will happen if the variable is declared 
-// // with const instead of let ?
-
-
-//==> On aurait une erreur quand funcTwo() essaie de modifier a, car une constante ne peut pas être réassignée.
-
-// /#3
-// function funcFour() {
-//     window.a = "hello";
-// }
-
-
-// function funcFive() {
-//     alert(`inside the funcFive function ${a}`);
-// }
-
-// // #3.1 - run in the console:
-// funcFour()
-// funcFive()
-
-// //#4
-// let a = 1;
-// function funcSix() {
-//     let a = "test";
-//     alert(`inside the funcSix function ${a}`);
-// }
-
-
-// // #4.1 - run in the console:
-// funcSix()
-// // #4.2 What will happen if the variable is declared 
-// // with const instead of let ?
-
-//==>
-//Pas de différence car on ne réassigne pas la variable locale. L'alerte afficherait toujours "inside the funcSix function test"
-
-
-// //#5
-// let a = 2;
-// if (true) {
-//     let a = 5;
-//     alert(`in the if block ${a}`);
-// }
-// alert(`outside of the if block ${a}`);
-
-// #5.1 - run the code in the console
-// #5.2 What will happen if the variable is declared 
-// with const instead of let ?
-
-//=>Meme comportement car les deux variables sont dans des portées différentes et ne sont pas réassignées dans leur portée respective.
+const color = () => {
+    colors.forEach((color, index) => {
+     console.log(`The #${index+1} choice is ${color}`);
+    });
+    if (colors.find(color => color === "Violet")) {
+     return "Yes";
+    }
+     return "No";
+ }
+ console.log(color());
